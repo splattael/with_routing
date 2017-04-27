e9d52c6f2daa1f4d251cc6001e032a5add8b05e0
Peter Suschlik <peter@suschlik.de>
2017-04-27 08:53:52 +0200

    Take trace probes

--- test/controllers/with_defaults_controller_test.rb
+++ test/controllers/with_defaults_controller_test.rb
@@ -1,22 +1,72 @@
 require 'test_helper'
+require 'pp'
 
 class WithDefaultsControllerTest < ActionController::TestCase
   test "passes for predefined routes" do
-    TracePoint.trace(:call) do |tp|
-      p tp.class
+    trace do
       get :index
       assert_response :success
     end
   end
 
-  test "fails for test routes" do
-    with_routing do |set|
-      set.draw do
-        get 'with_defaults2' => 'with_defaults#index'
-      end
+  # test "fails for test routes" do
+  #   with_routing do |set|
+  #     set.draw do
+  #       get 'with_defaults2' => 'with_defaults#index'
+  #     end
+  #
+  #     trace do
+  #       get :index
+  #       assert_response :success
+  #     end
+  #   end
+  # end
 
-      get :index
-      assert_response :success
+  private
+
+  def printme(tp, ident, prefix = "")
+    path = tp.path.gsub(/.*?gems\//, '')
+    path = path.gsub(Rails.root.to_s, '')
+    path = path.gsub("/home/peter/devel/rails/", '')
+    puts "%s%s%s#%s\t%s:%s" % ["  " * ident, prefix, tp.self.class, tp.method_id, path, tp.lineno]
+  end
+
+  def printparams(tp, ident)
+    method = tp.self.method(tp.method_id)
+    method.parameters.each do |p|
+      puts "%s - %s: %s" % ["  " * ident, p.last, obfusce(tp.binding.local_variable_get(p.last).pretty_inspect)]
     end
+  rescue
+    # raise
+    nil
+  end
+
+  def obfusce(x)
+    x.gsub(/0x[a-z0-9]{14}/, "0xXXXXXXXXXXXXX")
+  end
+
+  def trace
+    $ident = 0
+    tracers = []
+    tracers << TracePoint.new(:call) { |tp|
+      printme(tp, $ident)
+      printparams(tp, $ident)
+      $ident += 1
+    }
+
+    tracers << TracePoint.new(:return) { |tp|
+      printme(tp, $ident)
+      $ident -= 1
+    }
+
+    tracers << TracePoint.new(:raise) { |tp|
+      printme(tp, $ident, "RAISE")
+    }
+
+    tracers.each(&:enable)
+
+    yield
+  ensure
+    tracers.each(&:disable)
   end
 end
